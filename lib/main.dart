a50afe77e5fcbd2aac3c934f0fcc4ee894fa09e8
AabhasGaur19 <aabhasgaur14@gmai8l.com>
2025-01-18 15:48:05 +0530

    Locking orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:todo/widgets/welcome_screen.dart';
-import 'package:todo/widgets/todo_screen.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo/widgets/welcome_screen.dart';
+import 'package:todo/widgets/todo_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('en_IN', null);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) async {
+    await initializeDateFormatting('en_IN', null);
 
-  final prefs = await SharedPreferences.getInstance();
-  final String? username = prefs.getString('username'); 
+    final prefs = await SharedPreferences.getInstance();
+    final String? username = prefs.getString('username');
 
-  runApp(MyApp(initialScreen: username != null ? TodoScreen(username) : WelcomeScreen()));
+    runApp(MyApp(
+        initialScreen:
+            username != null ? TodoScreen(username) : const WelcomeScreen()));
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -22,10 +29,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.blueAccent[200],
+        scaffoldBackgroundColor: Colors.blue[200],
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: initialScreen, 
+      home: initialScreen,
     );
   }
 }
